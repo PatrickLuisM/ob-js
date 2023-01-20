9356061d547c0d60d9f576fa74435ddcca0eac0e
PatrickLuisM <patricklmo32@gmail.com>
2023-01-19 21:24:04 -0500

    Tarea 02

---
+++ Tarea02.js
@@ -0,0 +1,9 @@
+
+fecha = new Date(1995,3,08)
+libro = {
+    titulo: "Luna de Pluton",
+    autor: "Angel David Revilla",
+    fecha: 2016,
+    url: "https://www.dross.com/lunaDePluton"
+}
+lista = ["Patrick",27,true,fecha,libro]
