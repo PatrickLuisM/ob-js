20de4236c68b7bff156dd0901cd50bead01ad3e1
PatrickLuisM <patricklmo32@gmail.com>
2023-01-19 21:15:47 -0500

    Primeras clases

---
+++ Clase02.js
@@ -0,0 +1,17 @@
+//Tipos Primitivos
+
+//Number
+4
+//string
+"Hola mundo"
+'Hola mundo'
+
+//boleanos
+true
+false
+
+//nulo y undefined
+null
+undefined
+
+// null, undefined, false, 0
---
+++ Clase03.js
@@ -0,0 +1,11 @@
+
+var a = 1
+console.log(a)
+
+
+var a = 4
+console.log(a)
+
+///////////
+var num = 4
+console.log(typeof num)
